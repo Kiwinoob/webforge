70a5da2c0ed9c2d26fb8982433fbddf0875f85ae
Kiwinoob <Kiwi1@live.com.sg>
2025-05-10 16:14:31 +0800

    update FirgebaseConfig

--- backend/src/main/java/com/example/myapp/config/FirebaseConfig.java
+++ backend/src/main/java/com/example/myapp/config/FirebaseConfig.java
@@ -1,13 +1,15 @@
 package com.example.myapp.config;
 
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.DependsOn;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
@@ -15,33 +17,29 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 
-import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class FirebaseConfig {
 
-    @PostConstruct
-    public void initialize() {
-        try {
-            ClassPathResource resource = new ClassPathResource("firebase-service-account-key.json");
-            InputStream serviceAccount = resource.getInputStream();
-            
-            FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-            
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options); 
-            }
-            
-            System.out.println("Firebase initialization completed");
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
+    @Value("${FIREBASE_CONFIG}")
+    private String firebaseConfig;
+
     @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        InputStream serviceAccount = new ByteArrayInputStream(firebaseConfig.getBytes());
+        
+        FirebaseOptions options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        }
+        return FirebaseApp.getInstance();
+    }
+
+    @Bean
+    @DependsOn("firebaseApp")
     public Firestore firestore() {
         return FirestoreClient.getFirestore();
     }
