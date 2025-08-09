624065564cc1c75e55cf4889af638859596959da
quik214 <tzejunw@gmail.com>
2025-08-09 19:55:45 +0800

    Update Portfolio Hero Section

--- frontend/components/portfolio/hero-section.tsx
+++ frontend/components/portfolio/hero-section.tsx
@@ -11,9 +11,7 @@ export default function HeroSection() {
 
   const industries = [
     "Engineering",
-    "Professional Services", 
-    "Food & Beverages",
-    "Healthcare"
+    "Interior Design", 
   ];
 
   return (
