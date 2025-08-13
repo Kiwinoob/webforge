4f1652cf0ecf4ae4e1886617a2d242cc233dd5b8
Kiwinoob <50853758+Kiwinoob@users.noreply.github.com>
2025-08-14 02:12:32 +0800

    Update next.config.mjs
    revert changes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,34 +16,6 @@ const nextConfig = {
       },
     ],
   },
-  async redirects() {
-    return [
-      // Redirect from www to non-www version
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.webforge.sg', // Only trigger if the host is www.webforge.sg
-          },
-        ],
-        destination: 'https://webforge.sg/:path*', // Redirect to the non-www version
-        permanent: true, // Make it permanent
-      },
-      // Ensure HTTPS on non-www version (if it's HTTP)
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'webforge.sg', // Trigger only on non-www
-          },
-        ],
-        destination: 'https://webforge.sg/:path*', // Ensure HTTPS version
-        permanent: true, // Make it permanent
-      },
-    ];
-  },
 };
 
 export default nextConfig;
