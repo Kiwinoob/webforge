cec7a111a8b9c43c273639a193b5bc56930367e8
Kiwinoob <50853758+Kiwinoob@users.noreply.github.com>
2025-08-14 01:35:39 +0800

    Update next.config.mjs
    for google search console redirect errors

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,6 +16,28 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      // Redirect from HTTP to HTTPS
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'webforge.sg', // your domain without "www"
+          },
+        ],
+        destination: 'https://webforge.sg/:path*',
+        permanent: true,
+      },
+      // Redirect from www to non-www (optional)
+      {
+        source: 'http://www.webforge.sg/:path*',
+        destination: 'https://webforge.sg/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
